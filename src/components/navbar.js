30bfb48bb1f46ebdd3fe7383ecd2d0f4302d236c
Marissa Conner <talk@fairgroundcreative.com>
2018-08-06 14:57:10 -0600

    Tweak to fix navbar toggle button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,35 +9,37 @@ const Navbar = ({ changePage }) =>
         <img src="img/gold-mountain-gallery.jpg" alt="Gold Mountain Gallery" className="img-responsive" />
       </a>
     </div>
-    <button className="navbar-toggle" data-toggle="collapse" data-target="#gmg-nav">Open Close</button>
+    <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#gmg-nav-options">Open Close</button>
     
-    <nav className="collapsed navbar-collapse" id="gmg-nav" role="navigation">
+    <nav className="navbar" id="gmg-nav" role="navigation">
     
-      <ul className="nav navbar-nav">
-        <li className="dropdown">
-          <a href="#">About</a>
-          <ul className="dropdown-menu">
-            <li><a href="#">Our Artists</a></li>
-            <li><a href="#">Our Mission</a></li>
-          </ul>
-        </li>  
-        <li className="dropdown">
-          <a href="#">Beautiful Things</a>
-          <ul className="dropdown-menu">
-            <li><a href="#">Fine Artwork</a></li>
-            <li><a href="#">Jewelry</a></li>
-             <li><a href="#">Lighting</a></li>
-            <li><a href="#">Photography</a></li>
-            <li><a href="#">Woodworking</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Events</a>
-        </li>
-        <li>
-          <a href="#">Get In Touch</a>
-        </li>
-      </ul>
+      <div className="collapse navbar-collapse" id = "gmg-nav-options">
+        <ul className="nav navbar-nav">
+          <li className="dropdown">
+            <a href="#">About</a>
+            <ul className="dropdown-menu">
+              <li><a href="#">Our Artists</a></li>
+              <li><a href="#">Our Mission</a></li>
+            </ul>
+          </li>  
+          <li className="dropdown">
+            <a href="#">Beautiful Things</a>
+            <ul className="dropdown-menu">
+              <li><a href="#">Fine Artwork</a></li>
+              <li><a href="#">Jewelry</a></li>
+               <li><a href="#">Lighting</a></li>
+              <li><a href="#">Photography</a></li>
+              <li><a href="#">Woodworking</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+          <li>
+            <a href="#">Get In Touch</a>
+          </li>
+        </ul>
+      </div>
   
     </nav>
   </div>
