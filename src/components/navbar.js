e9dbfd03047e8ae603217b1f21dcf9fcbe4407ac
Marissa Conner <talk@fairgroundcreative.com>
2018-08-07 16:07:36 -0600

    Adjusting appearance for small desktop screens vs large

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,42 +9,42 @@ const Navbar = ({ changePage }) =>
         <img src="img/gold-mountain-gallery.jpg" alt="Gold Mountain Gallery" className="img-responsive" />
       </a>
     </div>
-    <button className="navbar-toggle collapsed btn btn-default" id="gmg-mobile-nav" data-toggle="collapse" data-target="#gmg-nav-options">
-      <span className="icon-bar top-bar"></span>
-      <span className="icon-bar middle-bar"></span>
-      <span className="icon-bar bottom-bar"></span>
-    </button>
-    
-    <nav className="navbar" id="gmg-nav" role="navigation">
-      <div className="collapse navbar-collapse" id = "gmg-nav-options">
-        <ul className="nav navbar-nav">
-          <li className="dropdown">
-            <a href="#">About</a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Our Artists</a></li>
-              <li><a href="#">Our Mission</a></li>
-            </ul>
-          </li>  
-          <li className="dropdown">
-            <a href="#">Beautiful Things</a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Fine Artwork</a></li>
-              <li><a href="#">Jewelry</a></li>
-               <li><a href="#">Lighting</a></li>
-              <li><a href="#">Photography</a></li>
-              <li><a href="#">Woodworking</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">Events</a>
-          </li>
-          <li>
-            <a href="#">Get In Touch</a>
-          </li>
-        </ul>
-      </div>
-  
-    </nav>
+    <div id="gmg-nav-menu">   
+      <button className="navbar-toggle collapsed btn btn-default" id="gmg-mobile-nav" data-toggle="collapse" data-target="#gmg-nav-options">
+        <span className="icon-bar top-bar"></span>
+        <span className="icon-bar middle-bar"></span>
+        <span className="icon-bar bottom-bar"></span>
+      </button>
+      <nav className="navbar" id="gmg-nav" role="navigation">
+        <div className="collapse navbar-collapse" id = "gmg-nav-options">
+          <ul className="nav navbar-nav">
+            <li className="dropdown">
+              <a href="#">About</a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Our Artists</a></li>
+                <li><a href="#">Our Mission</a></li>
+              </ul>
+            </li>  
+            <li className="dropdown">
+              <a href="#">Beautiful Things</a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Fine Artwork</a></li>
+                <li><a href="#">Jewelry</a></li>
+                 <li><a href="#">Lighting</a></li>
+                <li><a href="#">Photography</a></li>
+                <li><a href="#">Woodworking</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Events</a>
+            </li>
+            <li>
+              <a href="#">Get In Touch</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </div>
    
     
