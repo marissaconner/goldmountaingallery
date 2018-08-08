3929cd9202182ecbc202b1c58a501918af29fe25
Marissa Conner <talk@fairgroundcreative.com>
2018-08-08 12:47:11 -0600

    Added Matt Downer in artist data

---
+++ src/data/data-aritists.js
@@ -0,0 +1,61 @@
+const artists = {
+  [
+    {
+      "name"    : "Matt Downer",
+      
+      "image"   : "matt-downer-portrait.jpg",
+      
+      "bio"     : "<p>Matt Downer&rsquo;s work begs to be touched with its bold allure, hand-crafted contours, and nuanced detail that belies a lifetime of experience and craftsmanship. Ever since he was a kid making sketchy skateboard ramps and catapults, Matt has been fascinated with creating things that work.</p><p>Raised in Manhattan Beach, California, Matt graduated from CSU Chico with a Bachelors of Art in Philosophy and Religious Studies.  Soon after, he loaded his volkswagen to wander the west and landed in Telluride, Colorado, where he found and honed his skills at Steeprock Joinery.  Freed from the cramped cells that California apartments used to quarter him in, he finally was able to stretch out his wood lathe in the vibrant and verdant Rockies.  His contemporaries, like Christoph Neander, inspired him with museum-quality work to nuture an appreciation of his hand-made pieces by valuing the finest of details and every nuance of close inspection.</p><p>One of the great pleasures of working with wood is its the rich tactility.  &ldquo;My hope is that every one of my clients will caress each piece I&rsquo;ve made for them on a regular basis,&rdquo; Matt says. &ldquo;By feeling the curves and sensing the flow of the grain, the piece stands apart from a world full of objects spat out by some machine on the other side of the planet. Painstakingly crafted by attentive eyes and practiced hands, every surface of every piece I make is worked so as to bring life back to the wood that once grew as a beautiful tree.&rdquo;</p><p>Curved pieces with exposed joinery, pegged through-tenons, and artful dovetails compose a theme throughout Matt&rsquo;s designs, rendering each work unique not only to itself but to none other than Downer himself. Cherry, Maple and Black Walnut are his most popular materials, but any wood with the potential to enhance a custom-made piece may find itself in his hands, as well as metal, glass, stone and inlays of copper and silver.</p>",
+      
+      "path"    : "matt-downer",
+      
+      "gallery" : [
+        { 
+          "src" : "matt-downer-bed.jpg",
+          "alt" : "A bed"
+        },
+        { 
+          "src" : "matt-downer-blue-mobile.jpg",
+          "alt" : "A blue mobile"
+        },
+        { 
+          "src" : "matt-downer-chandelier.jpg",
+          "alt" : "A chandelier"
+        },
+        {
+          "src" : "matt-downer-cherry-sculpted-dining-chairs.jpg",
+          "alt" : "Cherry-wood sculpted dining chairs"
+        },
+        {
+          "src" : "matt-downer-drawers.jpg",
+          "alt" : "Custom set of drawers"
+        },
+        {
+          "src" : "matt-downer-headboard-detail.jpg",
+          "alt" : "A headboard"
+        },
+        {
+          "src" : "matt-downer-lamp-detail.jpg",
+          "alt" : "A closeup of a lamp"
+          
+        },
+        {
+          "src" : "matt-downer-lighting.jpg",
+          "alt" : "A chandelier with a rock accent"
+        },
+        {
+          "src" : "matt-downer-table.jpg",
+          "alt" : "A table"
+        },
+        {
+          "src" : "matt-downer-table-detail.jpg",
+          "alt" : "A detail of metal accent on a wooden table"
+        },
+        {
+          "src" : "matt-downer-table-lamp.jpg",
+          "alt" : "A table lamp"
+        },
+      ]
+    }
+  ]
+}
