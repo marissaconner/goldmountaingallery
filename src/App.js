15f9dcd37e393d9198712ad67829165035ca588c
Marissa Conner <talk@fairgroundcreative.com>
2018-08-09 00:47:34 -0600

    jake told me to

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/navbar.js';
-import PageContent from './components/pagecontent.js'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/navbar.js";
+import PageContent from "./components/pagecontent.js";
 
-class App extends Component { 
+class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      currentPage: 'home',
-    }
-    this.changePage = this.changePage.bind(this)
-  } 
+      currentPage: "home"
+    };
+    this.changePage = this.changePage.bind(this);
+  }
 
   changePage(currentPage) {
-    this.setState({ currentPage })
+    this.setState({ currentPage });
   }
 
   render() {
     return (
-     
-    <div>
-      <div className="container">
-        <header id="gmg-header">  
-          <Navbar changePage={this.changePage} />
-        </header>
-      </div>  
-            
-      <div id="gmg-body">
-        <PageContent currentPage={this.state.currentPage} />
+      <div>
+        <div className="container">
+          <header id="gmg-header">
+            <Navbar changePage={this.changePage} />
+          </header>
+        </div>
+
+        <div id="gmg-body">
+          <PageContent currentPage={this.state.currentPage} />
+        </div>
       </div>
-    </div>
     );
   }
 }
