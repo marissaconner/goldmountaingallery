4902ed8e3a27d6cd22dcb225586a5cc1f003ebc7
Marissa Conner <talk@fairgroundcreative.com>
2018-08-09 02:21:45 -0600

    Making it open the right carousel image on modal opening

--- src/components/artistgallery.js
+++ src/components/artistgallery.js
@@ -1,67 +1,112 @@
-import React  from 'react';
-    
-    
-  const GalleryEntry = ( { imgname, src, alt }) =>
-    <div className='gmg-artist-gallery-thumbnail' data-toggle='modal' data-target='#gmg-gallery-modal' >  
-      <img src={ 'img/artists/matt-downer/' + src }  className='img-responsive' alt = { alt } data-opens = {'#gallery-' + imgname } />
-    </div>
- 
-  const CarouselEntry = ({ imgname, src, alt}) =>
-    <div className="item">
-      <img src = { 'img/artists/matt-downer/' + src } alt = { alt } id= {'gallery-' + imgname } />
-    </div>
+import React from "react";
 
-   
-  const ArtistGallery = ({ artistData }) => 
-    <div>
-      <div className='row' id='gmg-artist-gallery'>
-        <div className='col-xs-12 text-center'>
-        
-          {artistData.map(
-            (galleryEntry, index) => 
-              <GalleryEntry key = {index} {...galleryEntry}/>
-          )}
-          
-        </div>
-      </div>
-      
-      <div className='center-center-container' >      
-        <div id='gmg-gallery-modal' className='modal fade' tabindex='-1' role='dialog'  aria-hidden='true'>
-        
-          <div className='vh-align-helper'>
-            <div className='modal-dialog vh-align-center' >
-              <div id='gmg-artist-carousel'   className='carousel slide carousel-fade'>
-                
-                <div className="carousel-inner">
-                  
-                  <div className="item active">
-                    <img src = 'img/artists/matt-downer/matt-downer-blue-mobile.jpg' alt = 'placeholder' />
-                  </div>
-                  
-                  {artistData.map(
-                    (galleryEntry, index) =>
-                      <CarouselEntry key = {index} {...galleryEntry} />
-                  )}
-                  
-                </div>
-       
-                <a className="left carousel-control" href="#gmg-artist-carousel" data-slide="prev">
-                  <span className="glyphicon glyphicon-chevron-left"></span>
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a className="right carousel-control" href="#gmg-artist-carousel" data-slide="next">
-                  <span className="glyphicon glyphicon-chevron-right"></span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </div>
+const Modal = ({ galleryItems, activeCarouselIndex }) => (
+  <div className="center-center-container">
+    <div
+      id="gmg-gallery-modal"
+      className="modal fade"
+      tabIndex="-1"
+      role="dialog"
+      aria-hidden="true"
+    >
+      <div className="vh-align-helper">
+        <div className="modal-dialog vh-align-center">
+          <div
+            id="gmg-artist-carousel"
+            className="carousel slide carousel-fade"
+          >
+            <div className="carousel-inner">
+              {galleryItems.map((galleryEntry, index) => (
+                <CarouselEntry
+                  key={index}
+                  isActive={index === activeCarouselIndex}
+                  {...galleryEntry}
+                />
+              ))}
             </div>
           </div>
-            
-       
         </div>
       </div>
-      
     </div>
+  </div>
+);
 
-  
-export default ArtistGallery;
+const GalleryEntry = ({ imgname, src, alt, clickHandler, index }) => (
+  <div
+    className="gmg-artist-gallery-thumbnail"
+    data-toggle="modal"
+    data-target="#gmg-gallery-modal"
+    onClick={() => {
+      clickHandler(index);
+    }}
+  >
+    <img
+      src={"img/artists/matt-downer/" + src}
+      className="img-responsive"
+      alt={alt}
+      data-opens={"#gallery-" + imgname}
+    />
+  </div>
+);
+
+const CarouselEntry = ({ imgname, src, alt, isActive }) => (
+  <div className={`item ${isActive ? "active" : ""}`}>
+    <img
+      src={"img/artists/matt-downer/" + src}
+      alt={alt}
+      id={"gallery-" + imgname}
+    />
+  </div>
+);
+
+const ArtistGallery = ({ clickHandler, galleryItems }) => (
+  <div className="row" id="gmg-artist-gallery">
+    <div className="col-xs-12 text-center">
+      {galleryItems.map((galleryEntry, index) => (
+        <GalleryEntry
+          key={index}
+          index={index}
+          clickHandler={clickHandler}
+          {...galleryEntry}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+class GalleryContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: {
+        activeCarouselIndex: 0
+      }
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <ArtistGallery
+          clickHandler={this.clickHandler}
+          galleryItems={this.props.galleryItems}
+        />
+        <Modal
+          galleryItems={this.props.galleryItems}
+          activeCarouselIndex={this.state.modal.activeCarouselIndex}
+        />
+      </div>
+    );
+  }
+
+  clickHandler(clickedIndex) {
+    this.setState({
+      modal: {
+        activeCarouselIndex: clickedIndex
+      }
+    });
+  }
+}
+
+export default GalleryContainer;
--- src/data/artistdata.js
+++ src/data/artistdata.js
@@ -1,50 +1,60 @@
-const artistdata = [
-        { 
-          "imgname" : "bed",
-          "src"  : "matt-downer-bed.jpg",
-          "alt"  : "A bed"
-        },
-        { 
-          "imgname" : "bluemobile",
-          "src" : "matt-downer-blue-mobile.jpg",
-          "alt" : "A blue mobile"
-        },
-        { 
-          "imgname" : "chandelier",
-          "src" : "matt-downer-chandelier.jpg",
-          "alt" : "A chandelier"
-        },
-        {
-          "imgname" : "sculpted-chairs",
-          "src" : "matt-downer-cherry-sculpted-dining-chairs.jpg",
-          "alt" : "Cherry-wood sculpted dining chairs"
-        },
-        {
-          "imgname" : "drawers",
-          "src" : "matt-downer-drawers.jpg",
-          "alt" : "Custom set of drawers"
-        },
-        {
-          "imgname" : "shelves",
-          "src" : "matt-downer-shelving.jpg",
-          "alt" : "Shelves"
-          
-        },
-        {
-          "imgname" : "lighting",
-          "src" : "matt-downer-lighting.jpg",
-          "alt" : "A chandelier with a rock accent"
-        },
-        {
-          "imgname" : "table",
-          "src" : "matt-downer-table.jpg",
-          "alt" : "A table"
-        },
-        {
-          "imgname" : "table-detail",
-          "src" : "matt-downer-table-detail.jpg",
-          "alt" : "A detail of metal accent on a wooden table"
-        }
-      ]
+const mattDowner = {
+  _id: 1,
+  firstName: "Matt",
+  lastName: "Downer",
+  bio: "<p>Bio Information</p><p>More Bio Information</p>",
+  tagline: "Custom Woodwork And Fine Furniture",
+  galleryItems: [
+    {
+      imgname: "bed",
+      src: "matt-downer-bed.jpg",
+      alt: "A bed"
+    },
+    {
+      imgname: "bluemobile",
+      src: "matt-downer-blue-mobile.jpg",
+      alt: "A blue mobile"
+    },
+    {
+      imgname: "chandelier",
+      src: "matt-downer-chandelier.jpg",
+      alt: "A chandelier"
+    },
+    {
+      imgname: "sculpted-chairs",
+      src: "matt-downer-cherry-sculpted-dining-chairs.jpg",
+      alt: "Cherry-wood sculpted dining chairs"
+    },
+    {
+      imgname: "drawers",
+      src: "matt-downer-drawers.jpg",
+      alt: "Custom set of drawers"
+    },
+    {
+      imgname: "shelves",
+      src: "matt-downer-shelving.jpg",
+      alt: "Shelves"
+    },
+    {
+      imgname: "lighting",
+      src: "matt-downer-lighting.jpg",
+      alt: "A chandelier with a rock accent"
+    },
+    {
+      imgname: "table",
+      src: "matt-downer-table.jpg",
+      alt: "A table"
+    },
+    {
+      imgname: "table-detail",
+      src: "matt-downer-table-detail.jpg",
+      alt: "A detail of metal accent on a wooden table"
+    }
+  ]
+};
 
-export default artistdata;
+const artistData = {
+  [mattDowner._id]: mattDowner
+};
+
+export default artistData;
