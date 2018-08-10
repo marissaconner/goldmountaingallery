c2a5f022a95e88c1cd93c11d7b2fa44933cbd5e9
Marissa Conner <talk@fairgroundcreative.com>
2018-08-10 14:12:23 -0600

    Editing artist data and artist components

--- src/components/artist.js
+++ src/components/artist.js
@@ -1,5 +1,7 @@
 import React from "react";
 import GalleryContainer from "./artistgallery.js";
+import Parser from "html-react-parser";
+  
 
 const Artist = ({ artistData }) => (
   <article>
@@ -8,14 +10,13 @@ const Artist = ({ artistData }) => (
         <div
           id="gmg-artist-heading"
           style={{
-            backgroundImage:
-              'url("img/artists/matt-downer/matt-downer-banner.jpg")'
+            backgroundImage: {artistData.bannerUrl}
           }}
         >
-          <h1 id="gmg-artist-name">Matt Downer</h1>
-          <span id="gmg-artist-tagline">
-            Custom Woodwork And Fine Furniture
-          </span>
+          <h1 id="gmg-artist-name">
+            {artistData.firstName} {artistData.lastName}
+          </h1>
+          <span id="gmg-artist-tagline">{artistData.tagline}</span>
         </div>
       </div>
     </div>
@@ -24,58 +25,14 @@ const Artist = ({ artistData }) => (
       <section>
         <div className="row">
           <div className="col-md-12">
-            <h2 className="text-center">About Matt</h2>
-
+            <h2 className="text-center">About {artistData.firstName}</h2>
             <img
               id="gmg-artist-portrait"
               className="col-md-5 col-sm-12 pull-left img-responsive"
-              src="img/artists/matt-downer/matt-downer-portrait.jpg"
-              alt="Firstname Lastname"
+              src={artistData.portraitSrc}
+              alt=
             />
-
-            <p>
-              Matt Downer's work begs to be touched with its bold allure,
-              hand-crafted contours, and nuanced detail that belies a lifetime
-              of experience and craftsmanship. Ever since he was a kid making
-              sketchy skateboard ramps and catapults, Matt has been fascinated
-              with creating things that work.
-            </p>
-
-            <p>
-              Raised in Manhattan Beach, California, Matt graduated from CSU
-              Chico with a Bachelors of Art in Philosophy and Religious Studies.
-              Soon after, he loaded his volkswagen to wander the west and landed
-              in Telluride, Colorado, where he found and honed his skills at
-              Steeprock Joinery. Freed from the cramped cells that California
-              apartments used to quarter him in, he finally was able to stretch
-              out his wood lathe in the vibrant and verdant Rockies. His
-              contemporaries, like Christoph Neander, inspired him with
-              museum-quality work to nuture an appreciation of his hand-made
-              pieces by valuing the finest of details and every nuance of close
-              inspection.
-            </p>
-
-            <p>
-              One of the great pleasures of working with wood is its the rich
-              tactility. &ldquo;My hope is that every one of my clients will
-              caress each piece I&rsquo;ve made for them on a regular
-              basis,&rdquo; Matt says. &ldquo;By feeling the curves and sensing
-              the flow of the grain, the piece stands apart from a world full of
-              objects spat out by some machine on the other side of the planet.
-              Painstakingly crafted by attentive eyes and practiced hands, every
-              surface of every piece I make is worked so as to bring life back
-              to the wood that once grew as a beautiful tree.&rdquo;
-            </p>
-
-            <p>
-              Curved pieces with exposed joinery, pegged through-tenons, and
-              artful dovetails compose a theme throughout Matt&rsquo;s designs,
-              rendering each work unique not only to itself but to none other
-              than Downer himself. Cherry, Maple and Black Walnut are his most
-              popular materials, but any wood with the potential to enhance a
-              custom-made piece may find itself in his hands, as well as metal,
-              glass, stone and inlays of copper and silver.
-            </p>
+            {Parser(artistData.bioText)}
           </div>
         </div>
       </section>
--- src/data/artistdata.js
+++ src/data/artistdata.js
@@ -2,8 +2,11 @@ const mattDowner = {
   _id: 1,
   firstName: "Matt",
   lastName: "Downer",
-  bio: "<p>Bio Information</p><p>More Bio Information</p>",
+  bioText:
+    "<p>Matt Downer's work begs to be touched with its bold allure, hand-crafted contours, and nuanced detail that belies a lifetime of experience and craftsmanship. Ever since he was a kid making sketchy skateboard ramps and catapults, Matt has been fascinated with creating things that work.</p><p>Raised in Manhattan Beach, California, Matt graduated from CSUChico with a Bachelors of Art in Philosophy and Religious Studies.Soon after, he loaded his volkswagen to wander the west and landedin Telluride, Colorado, where he found and honed his skills atSteeprock Joinery. Freed from the cramped cells that Californiaapartments used to quarter him in, he finally was able to stretchout his wood lathe in the vibrant and verdant Rockies. Hiscontemporaries, like Christoph Neander, inspired him withmuseum-quality work to nuture an appreciation of his hand-madepieces by valuing the finest of details and every nuance of closeinspection.</p><p>One of the great pleasures of working with wood is its the richtactility. &ldquo;My hope is that every one of my clients willcaress each piece I&rsquo;ve made for them on a regularbasis,&rdquo; Matt says. &ldquo;By feeling the curves and sensingthe flow of the grain, the piece stands apart from a world full ofobjects spat out by some machine on the other side of the planet.Painstakingly crafted by attentive eyes and practiced hands, everysurface of every piece I make is worked so as to bring life backto the wood that once grew as a beautiful tree.&rdquo;</p><p>Curved pieces with exposed joinery, pegged through-tenons, andartful dovetails compose a theme throughout Matt&rsquo;s designs,rendering each work unique not only to itself but to none otherthan Downer himself. Cherry, Maple and Black Walnut are his mostpopular materials, but any wood with the potential to enhance acustom-made piece may find itself in his hands, as well as metal,glass, stone and inlays of copper and silver.</p>",
   tagline: "Custom Woodwork And Fine Furniture",
+  bannerSrc: "img/artists/matt-downer/matt-downer-banner.jpg",
+  portraitSrc: "img/artists/matt-downer/matt-downer-portrait.jpg",
   galleryItems: [
     {
       imgname: "bed",
