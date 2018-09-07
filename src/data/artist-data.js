876c3cc4cf663b89459ccf7036ed0b6a9178a46d
Marissa Conner <talk@fairgroundcreative.com>
2018-09-06 17:07:41 -0700

    Michelle's photos and alt descriptions

--- src/data/artist-data.js
+++ src/data/artist-data.js
@@ -4,16 +4,46 @@ const michelleMarocco = {
   lastName: "Marocco",
   bioText:
     "<p>Even if you have never met Michelle before, you may have seen her work already on celebrities like Russell Brand, Nicole Scherzinger, Elaine Irwin, or Frankie Ballard.  Michelle Marocco has earned international acclaim as a jewelry artist, having been personally invited to Saudi Arabia by the royal family to exhibit the Marocco collection at an annual women-only charity event.  Her one-of-a-kind works of wearable art are in high demand, inspired by meditation and her yoga practice. Although yoga is a strong influence in her work, it is her family which is the deepest root of all of her inspiration and which she puts forst.  Her two sons, Gavin and Justin, always motivate her to design and create.</p><p>Michelle started making jewelry eight years ago. Five years ago, she forged the guiding concept of &ldquo;The Audacity Of Diamonds On Leather&rdquo;. To this day, each of her handcrafted pieces adhere to these conceptual roots, crafted to be a piece of wearable artwork infused with the contemplative energy of the creator.</p><p>Michelle&rsquo;s passion in life is to be in the moment of the creative process, driven to capture and harness that compelling urgency to create works of art that channel positive energy and help rejuvenate the soul.  The Marocco line includes numerous, unique collections born out of her multidimensional imagination: as she describes it, &ldquo;I like to style both the body and spirit with my jewelry.&rdquo;/p><p> Michelle&rsquo;s talents also extend to paintings: she is an accomplished abstract-expressionist and figurative painter with a 15-year career on canvas.   The dialogue braided into her paintings narrates, with layers of textures and symbols, the complex history and influences that shape us.  This theme comes from her reverence for the life principle of Niyama, or purification through self-discipline, a part of the eightfold path of yoga.</p>",
-  tagline: "Diamond and leather jewelry",
-  bannerSrc: "img/artists/michelle-marocco/michelle-marocco-banner.jpg",
-  portraitSrc: "img/artists/michelle-marocco/michelle-marocco-portrait.jpg",
+  tagline: "The Audacity Of Diamonds On Leather",
+  bannerSrc: "/img/artists/michelle-marocco/michelle-marocco-banner.jpg",
+  portraitSrc: "/img/artists/michelle-marocco/michelle-marocco-portrait.jpg",
   galleryItems: [
     {
-      imgname: "Sacred Buddha Bracelet",
-      src:
-        "/img/artists/michelle-marocco/michelle-marocco-sacred-buddha-necklace.jpg",
+      imgname: "BusyGirl Pendants",
+      src: "michelle-marocco-pendants.jpg",
       alt:
-        "Black Sacred Buddha necklace by Michelle Marocco. These Mala bead meditation-inspired necklaces feature a Nepalese Buddha relic, carved wood, labradorite, african metal and other complementary beads."
+        "A collection of several beautiful pendants by Michelle Marocco, part of her BusyGirl and Niyama collections."
+    },
+    {
+      imgname: "Diamond Shaker Pendant",
+      src: "michelle-marocco-diamond-shaker-pendant.jpg",
+      alt:
+        "A unique pendant by Michelle Marocco that boasts 1.8 karats of brilliant diamonds. Part of her BusyGirl collection."
+    },
+    {
+      imgname: "Michelle Marocco BusyGirl Bracelets",
+      src: "michelle-marocco-bracelets.jpg",
+      alt:
+        "A collection of bracelets by Michelle Marocco, featuring sapphire and snakeskin for a layered look and paired with silver and diamond bands."
+    },
+    {
+      imgname: "Curled Feather Diamond Pendant",
+      src: "michelle-marocco-curled-diamond-feather-pendant.jpg",
+      alt:
+        "A feather diamond pendant.  Part of Michelle Marocco's BusyGirl collection."
+    },
+    {
+      imgname: "Arrowhead Pendant",
+      src:
+        "michelle-marocco-labradorite-and-feather-choker-with-arrowhead-pendant.jpg",
+      alt:
+        "Another piece from Michelle Marocco's BusyGirl collection, this one featuring an arrowhead pendant and a labradorite-and-silver  choker."
+    },
+    {
+      imgname: "Friend Of Buddha Necklace",
+      src: "michelle-marocco-friend-of-buddha-necklace.jpg",
+      alt:
+        "Friend Of Buddha necklace by Michelle Marocco. A beaded necklace with a buddha pendant, part of the Niyama by Marocco collection."
     },
 
     {
@@ -21,6 +51,37 @@ const michelleMarocco = {
       src: "michelle-marocco-stitched-leather-bead-bracelet.jpg",
       alt:
         "A stitched leather bead bracelet by Michelle Marocco. The bracelet is made with a magnetic clasp for easy wearing and removal."
+    },
+    {
+      imgname: "Labradodrite Bezel Choker Necklace",
+      src: "michelle-marocco-labradorite-bezel-choker.jpg",
+      alt:
+        "Minimalist design with maximum style by Michelle Marocco. You will get lost in the mysterious depths of this faceted labradorite necklace."
+    },
+    {
+      imgame: "Diamond Love Necklace",
+      src: "michelle-marocco-diamond-love-and-coin-necklace.jpg",
+      alt:
+        "A hand-crafted necklace by Michelle Marocco. Wear this necklace alone for a simple statement, or compliment it with other pieces."
+    },
+    {
+      imgname: "Diamond Snake Pendant",
+      src: "michelle-marocco-snake-pendant.jpg",
+      alt:
+        "A pav&eacute; diamond snake pendant by Michelle Marocco as part of her BusyGirl collection, paired with a bronze dome with a rich patina."
+    },
+    {
+      imgname: "Men's textured tile braelet",
+      src: "michelle-marocco-bronze-textured-tile-bracelet.jpg",
+      alt:
+        "Men's bracelet by Michelle Marocco. Part of her ManGear collection.  This bracelet features a kiln-fired textured tile with a strand of crocheted leather."
+    },
+    {
+      imgname: "Silver Textured Tile Bracelet",
+      src:
+        "michelle-marocco-fire-agate-and-pyrite-beaded-bracelet-with-silver-tile.jpg",
+      alt:
+        "Men's bracelet by Michelle Marocco as part of her ManGear collection. A textured sterling silver tile pairs with strips of black leather for a smart and sophisticated look."
     }
   ]
 };
