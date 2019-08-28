ec9f59751e1fc627ad6dbc0f614554105cf93554
Marissa Conner <talk@fairgroundcreative.com>
2019-08-27 17:18:41 -0700

    replacing elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,82 @@
 import React from "react";
 
 const Navbar = () => (
-  <div className="container" id="gmg-header">
-    <div id="gmg-masthead">
-      <a href="/">
-        <img
-          src="/img/gold-mountain-gallery.jpg"
-          alt="Gold Mountain Gallery"
-          className="img-responsive"
-        />
+  <header className="navbar" role="navigation">
+  
+    <div className="container d-flex w-100 header navbar__brand">
+      <div className="menu-icon-mobile d-md-none navbar__brand__menu-toggle closed open">
+        <svg data-src="/img/mobile-menu.svg" width="50px" height="31px"/>
+      </div>
+
+      <a href="https://www.goldmountaingallery.com/" className="navbar__logo">
+        <img src="/img/gold-mountain-gallery.jpg"/>
       </a>
-    </div>
-    <div id="gmg-nav-menu">
-      <button
-        className="navbar-toggle collapsed btn"
-        id="gmg-mobile-nav"
-        data-toggle="collapse"
-        data-target="#gmg-nav-options"
-      >
-        <span className="icon-bar top-bar" />
-        <span className="icon-bar middle-bar" />
-        <span className="icon-bar bottom-bar" />
-      </button>
-      <nav className="navbar" id="gmg-nav">
-        <div className="collapse navbar-collapse" id="gmg-nav-options">
-          <ul className="nav navbar-nav">
-            <li className="dropdown">
-              <a href="/about">About Us</a>
-              {/*<ul className="dropdown-menu">
-                <li>
-                  <a href="/about">About Us</a>
-                </li>
-                 <li>
-                  <a href="/artists">Meet Our Artists</a>
-                </li> 
-              </ul>*/}
+
+      <div className="navbar__search">
+        <form className="form-inline" id="homepage-search" action="https://www.goldmountaingallery.com/search">
+          <input className="form-control w-100" type="text" name="q" placeholder="Search" required />
+          <button className="btn btn-search" type="submit">
+            <svg data-src="/img/search.svg" width="18" height="18"/>
+          </button>
+        </form>
+      </div>
+
+      <div className="navbar__menu navbar__menu--topmenu">
+        <nav className="navigation closed">
+          <ul className="menu nav mb-4 mb-md-0">
+            <li className="nav-item menu__item">
+              <a className="nav-link" href="https://www.goldmountaingallery.com/page/about">About Us</a>
             </li>
-            <li>
-              <a href="/gallery">Gallery</a>
+
+            <li className="nav-item menu__item">
+              <a className="nav-link" href="#">Contact</a>
             </li>
-            <li>
-              <a href="/contact">Get In Touch</a>
+
+            <li className="nav-item menu__item">
+              <a className="nav-link" href="https://www.goldmountaingallery.com/account/signup">Register</a>
+            </li>
+
+            <li className="nav-item menu__item">
+              <a className="nav-link" href="https://www.goldmountaingallery.com/account/login">Log In</a>
             </li>
           </ul>
+        </nav>
+      </div>
+
+      <div className="checkout__icon">
+        <div className="navbar__brand__checkout__icon" style={{width:62, height:28, position:'relative' }}>
+          <a rel="nofollow" href="https://www.goldmountaingallery.com/checkout">
+            <svg data-src="/img/checkout.svg" width="62" height="28"/>
+          </a>
         </div>
-      </nav>
+      </div>    
+
+      <div className="navbar__menu navbar__menu--topmenu d-block mb-md-0">
+        <nav className="navigation">
+            <ul className="menu nav mb-4 mb-md-0">
+             <div className="d-md-none">
+              <li className="nav-item menu__item">
+                <a className="nav-link" href="https://www.goldmountaingallery.com/page/about">About Us</a>
+              </li>
+
+              <li className="nav-item menu__item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+
+              <li className="nav-item menu__item">
+                <a className="nav-link" href="https://www.goldmountaingallery.com/account/signup">Register</a>
+              </li>
+
+              <li className="nav-item menu__item">
+                <a className="nav-link" href="https://www.goldmountaingallery.com/account/login">Log In</a>
+              </li>
+            </div>        
+          </ul>
+        </nav>
+      </div>
     </div>
-  </div>
+  </header>
+
 );
 
 export default Navbar;
